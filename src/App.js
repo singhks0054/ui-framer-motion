9c39f6b36509a0e524bc1f4ae29ab2346d25c0d2
Krishna <singhks0054@gmail.com>
2023-12-08 11:30:54 +0530

    Add react-router-dom and create email page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import BackGround from './components/background';
 import Foreground from './components/foreground';
+import Email from './page/email';
 
 function App() {
   return (
     <>
-      <BackGround />
-      <Foreground />
+      <Routes>
+        <Route
+          path='/'
+          exact
+          element={
+            <>
+              <BackGround />
+              <Foreground />
+            </>
+          }
+        />
+        <Route path='/email' exact Component={Email} />
+      </Routes>
     </>
   );
 }
---
+++ src/page/email.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Email() {
+  return (
+    <>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: 0,
+          paddingTop: '200.0000%',
+          paddingBottom: 0,
+          boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
+          marginTop: '1.6em',
+          marginBottom: '0.9em',
+          overflow: 'hidden',
+          borderRadius: '8px',
+          willChange: 'transform',
+        }}
+      >
+        <iframe
+          loading='lazy'
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+            border: 'none',
+            padding: 0,
+            margin: 0,
+          }}
+          src='https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAF2XP_RXcE&#x2F;view?embed'
+          allowfullscreen='allowfullscreen'
+          allow='fullscreen'
+          title='Your Career Upgrade Awaits with CU Online &amp; KPMG!'
+        ></iframe>
+      </div>
+    </>
+  )
+}
